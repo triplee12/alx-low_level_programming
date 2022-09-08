7fb21d47de8d3ef403e5193f7833ba8b17e7d9fa
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-09-08 08:04:17 -0700

    Add puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,9 +10,7 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	puts(Mystr);
-
+	puts("\"Programming is like building a multilingual     puzzle");
 	return (0);
 }
 
