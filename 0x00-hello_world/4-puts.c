902960627a8cec45aac189c15a27a095518043d9
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-09-08 06:40:36 -0700

    Add c file that prints string in a newline

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,14 @@
 
 /**
  * main - Print string to a newline using puts function
+ *
  * Description: Using puts function to print string in a newline
+ *
  * Return: 1 if error occurs. 0 otherwise
  */
 
 int main(void)
 {
-
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 	return (0);
