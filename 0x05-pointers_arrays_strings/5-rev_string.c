464c26e1a37c9d36f2acde1ee13040e0c318fa24
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-09-19 09:33:20 -0700

    Reverse string and store it in a pointer valiable

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string store in a valiable pointer
+ * @s: valiable pointer that store the character string
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	char *ptr_s;
+
+	ptr_s = s;
+
+	while (s[i])
+		i++;
+	while (i--)
+		ptr_s[i] = s[i]; 
+}
