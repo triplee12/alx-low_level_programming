f6518a42b186b85db3f14d837810510bd381ebb7
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-09-26 10:42:46 -0700

    Locate character constant in a pointer string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr(char *s, char c) - print a character string from a pointer string
+ * @s: pointer string to locate a character from
+ * @c: character constant to search for in a pointer string
+ * Return: the first occurrence of the character constant
+ */
+
+char *_strchr(char *s, char c)
+{
+	do
+	{
+		if (*s == c)
+			return (char*)s;
+	}
+	while (*s++);
+	return (s);
+}
