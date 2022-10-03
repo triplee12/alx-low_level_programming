e5f899aeb2826306f265996196149c87d2c9e3a8
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-10-03 12:43:22 -0700

    Fix betty coding style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,28 +10,32 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int s1_len = 0;
-	unsigned int s2_len = 0;
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
 	char *ptr_mem;
 	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 
-	while (*(s1 + s1_len) != '\0')
-		s1_len++;
+	if (s2 == NULL)
+		s2 = "";
 
-	while (*(s2 + s2_len) != '\0')
-		s2_len++;
+	while (*(s1 + s1_length) != '\0')
+		s1_length++;
 
-	ptr_mem = malloc(1 + (s1_len * sizeof(*s1)) + (s2_len * sizeof(*s2)));
+	while (*(s2 + s2_length) != '\0')
+		s2_length++;
+
+	ptr_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
 	if (ptr_mem == NULL)
-		return '\0';
+		return ('\0');
 
-	for (i = 0; i < s1_len; i++)
+	for (i = 0; i < s1_length; i++)
 		ptr_mem[i] = *(s1 + i);
 
-	for (j = 0; j < s2_len; j++)
+	for (j = 0; j < s2_length; j++, i++)
 		ptr_mem[i] = *(s2 + j);
 
 	return (ptr_mem);
+}
