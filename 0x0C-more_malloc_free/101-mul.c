8d46ce1a5a3552336ce106c3e2ff5d2eea0d9506
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-10-06 07:44:12 -0700

    Multiplies two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument value
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+
+unsigned long mul;
+int i, j;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
+
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+}
