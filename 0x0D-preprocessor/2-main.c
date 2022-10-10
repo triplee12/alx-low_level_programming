840c5b138a52fbec937491a267d15c8431b90e3c
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-10-10 13:26:15 -0700

    Print the name of the file program was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - starting point of the program
+ * Return: 0 on success, otherwise error code
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
