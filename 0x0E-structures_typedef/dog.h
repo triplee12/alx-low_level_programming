406e3b603997eaed5a43f5b3b20a8f260cac20d8
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-10-10 14:02:03 -0700

    Add more file to the dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,28 @@
-#ifndef
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
 
-typedef struct dog {char *name, float age, char *owner} Dog;
+/**
+ * struct dog - structure dog
+ * @name: member pointer name to dog
+ * @age member age to dog
+ * @owner: member pointer owner to dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
