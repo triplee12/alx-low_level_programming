e593274214ad096c7012dc13a2bd3eb2152a4d12
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-10-13 13:06:28 -0700

    Print the sum of variadic function arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - prints the sum of it's arguments
+ *
+ * @n: the number of arguments entered
+ *
+ * Return: sum of the arguments
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum;
+
+	va_start (ap, n);
+
+	sum = 0;
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg (ap, unsigned int);
+
+	va_end (ap);
+
+	return (sum);
+}
