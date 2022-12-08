85b3cbf4e59293a05a54e034829eb68708ee6f3c
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-12-08 11:47:08 -0800

    Fix code

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,7 +6,7 @@
  * @n: node data
  * Return: new node
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *temp_node, *new_node;
 
